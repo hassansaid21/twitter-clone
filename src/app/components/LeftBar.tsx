de59c948d1e5d2ced1bf0fa339a8863039816f39
Hassan Said <karimsaid214@gmail.com>
2025-12-28 18:44:14 +0200

    add  custom  break points

---
+++ src/app/components/LeftBar.tsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function LeftBar() {
+  return (
+    <div>LeftBar</div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import LeftBar from "./components/LeftBar";
+import RightBar from "./components/RightBar";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +9,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex justify-between max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-xxl mx-auto transition-all  ">
+          <div className="px-2 xsm:px-4 xxl:px-8">
+            <LeftBar />
+          </div>
+          <div className="flex-1 ml-4 md:ml-8 border-x-[1px] min-h-screen border-gray-100 lg:min-w-[600px]">{children}</div>
+
+          <div className="hidden lg:flex flex-1">
+            <RightBar />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
