1ae08c9b84245bb3a9f4e62bb964f89c24312d2b
Hassan Said <karimsaid214@gmail.com>
2025-12-29 19:42:38 +0200

    finished LeftBar component

--- src/app/components/LeftBar.tsx
+++ src/app/components/LeftBar.tsx
@@ -1,9 +1,111 @@
+import Image from "next/image";
+import Link from "next/link";
 
-
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
 
 
 export default function LeftBar() {
   return (
-    <div>LeftBar</div>
+    <nav aria-label="primary" className="flex  sticky top-0 h-screen flex-col justify-between   py-2">
+       <div className="flex flex-col gap-1.5  text-md items-center xxl:items-start ">
+           <Link className="p-3 rounded-full  hover:bg-[#181818]" href='/' aria-label="Go to homepage">
+              <Image src='icons/logo.svg'  alt="X" width={24} height={24}/>
+           </Link>
+           <ul className="flex flex-col gap-1 " role="list">
+                {menuList.map((item)=>(
+                  <li key={item.id}>
+
+                   <Link aria-label={item.name} className="flex items-center gap-4 p-3 rounded-full hover:bg-[#181818] "  href={item.link}>
+                      <Image src={`icons/${item.icon}`} width={24} height={24}  alt='' aria-hidden="true" />
+                      <span className="hidden xxl:inline">{item.name}</span>
+                  </Link>
+                  </li>
+               ))}
+
+       
+           </ul>
+        
+        <Link className="bg-white rounded-full  p-2  xxl:hidden  " href='/'>
+        <Image src='icons/post.svg' alt="post-icon" width={24} height={24}/>
+        </Link>
+        <Link href='/' className="hidden xxl:inline bg-white text-lg font-bold rounded-full py-2 px-20  text-black text-center" >
+          Post</Link>
+        </div>
+        
+        
+        <div aria-haspopup="menu"  className="flex items-center p-2  justify-between cursor-pointer hover:bg-[#181818] rounded-full " role="button" aria-label="Open profile menu">
+             <div className="flex items-center  gap-2">
+              <div className="relative w-10 h-10 rounded-full overflow-hidden ">
+                <Image src='/general/me.png' alt="user-logo" fill/>
+              </div>
+              <div className="hidden xxl:flex flex-col   text-sm ">
+                <span className="font-bold">Hassan Said</span>
+                <span className="text-textGray">@hassansaid21</span>
+              </div>
+             </div>
+              <div className="text-2xl  hidden xxl:block " aria-hidden="true">...</div>
+     </div>
+    </nav>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,21 +6,22 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) 
+
+{
   return (
     <html lang="en">
       <body>
-        <div className="flex justify-between max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-xxl mx-auto transition-all  ">
-          <div className="px-2 xsm:px-4 xxl:px-8">
-            <LeftBar />
-          </div>
-          <div className="flex-1 ml-4 md:ml-8 border-x-[1px] min-h-screen border-gray-100 lg:min-w-[600px]">{children}</div>
+  
+<div className="flex justify-between md:max-w-screen-md lg:max-w-screen-lg  xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto ">
+  <div className="px-4 xxl:px-8  "><LeftBar/></div>
+  <div className="flex-1 border-x-[1px] border-gray-100  lg:min-w-[600px] min-h-screen ">{children}</div>
+  <div className="hidden lg:flex flex-1 md:ml-8  "><RightBar /></div>
+</div>
 
-          <div className="hidden lg:flex flex-1">
-            <RightBar />
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+
+</body>
+</html>
+
+  ) 
 }
